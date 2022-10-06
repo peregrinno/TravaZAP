4e5244a377fe9a046689f3b158f2a067f09a8433
Luan Araújo <joseluan74@gmail.com>
2022-10-06 13:37:20 +0000

    Finalizei o codigo

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
-import pywhatkit
-import keyboard
+import pyautogui
 import time
+'''
+COLOQUE AS POSIÇÕES NECESSÁRIAS PARA O BOT FUNCIONAR
+x, y = pyautogui.position() 
+print(x,y)
+'''
+#ABA ZAPZAP
+pyautogui.moveTo(947,12)
+pyautogui.click()
+time.sleep(2)
 
-def __init__():
-    print('Tudo ok :)')
+#BARRA DE BUSCA
+pyautogui.moveTo(103, 153)
+pyautogui.click()
+time.sleep(2)
 
+#NOME DO GRUPO, (NÃO PRECISA SER COMPLETO)
+pyautogui.typewrite('')
+time.sleep(4)
+pyautogui.moveTo(216,288)
+pyautogui.click()
+time.sleep(2)
 
-__init__()
+#MENSAGEM 1
+pyautogui.typewrite('')
+pyautogui.press('enter')
+time.sleep(5)
+
+#LOOP DE ENVIOS
+for i in range(0,100):
+    pyautogui.typewrite('')
+    pyautogui.press('enter')
